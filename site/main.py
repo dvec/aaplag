e9840e1a2fe19bcf957e36b1aac39acc2a0d6c68
Megageorgio <korogodoff@yandex.ru>
2019-04-06 21:49:07 +0400

    site

---
+++ site/main.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from flask import Flask, request, render_template
+app = Flask(__name__)
+tkey = "trnsl.1.1.20190406T120126Z.f880cac48e1e6bdb.ce1ea65ddd619cac01e53e80bead80c5edf56401"
+turl = "https://translate.yandex.net/api/v1.5/tr.json/translate"
+def translate_text(text, lang):
+	return json.loads(requests.get(turl, params = {'key':tkey, 'lang':lang, 'text': text}).text)['text'][0]
+@app.route('/', methods=['GET', 'POST'])
+def mainpage():
+	if request.method == 'POST':
+		input = request.form.get('input')
+		output = translate_text(input, 'ru-en')
+		output = translate_text(output, 'en-ru')
+		return render_template("main.html", input=input, output=output)
+	else:
+		return render_template("main.html")
+
+if __name__ == '__main__':
+    app.run(port=80)
